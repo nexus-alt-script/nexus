76e1c4304461538c4156281ea218e28032f77db8
nexus-alt-script <zyrus.carl.pajanel@gmail.com>
2024-07-12 23:20:25 +0800

    Update joinNoti.js

--- modules/events/joinNoti.js
+++ modules/events/joinNoti.js
@@ -6,8 +6,8 @@ module.exports.config = {
   description: "GROUP UPDATE NOTIFICATION"
 };
 
-const ADMIN = 'YOUR_NAME';
-const FB_LINK = 'YOUR_FACEBOOK_LINK';
+const ADMIN = 'Nexus';
+const FB_LINK = 'N/A';
 
 const fs = require('fs-extra');
 const { loadImage, createCanvas, registerFont } = require("canvas");
